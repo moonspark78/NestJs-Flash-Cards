8354e4a0aac2bd2c1a1df58b3f7c85a0b52140d3
moonspark78 <115048076+moonspark78@users.noreply.github.com>
2023-04-11 12:22:25 +0200

    :building_construction:  create & setup Card controller  :building_construction:

--- src/cards/cards.controller.ts
+++ src/cards/cards.controller.ts
@@ -1,4 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+import { CardsService } from './cards.service';
+import { Card } from 'src/interfaces/card/card.interface';
 
 @Controller('cards')
-export class CardsController {}
+export class CardsController {
+  constructor(private cardsService: CardsService) {}
+
+  @Get()
+  async getCards(): Promise<Card[]> {
+    const cards = await this.cardsService.getCards();
+    return cards;
+  }
+}
