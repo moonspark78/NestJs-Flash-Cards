b8dceacbab77ebef951821cc13bcdef9d4e10bee
moonspark78 <115048076+moonspark78@users.noreply.github.com>
2023-04-11 12:10:57 +0200

    :pencil2: create a Card interface  :pencil2:

--- src/cards/cards.service.ts
+++ src/cards/cards.service.ts
@@ -1,4 +1,19 @@
 import { Injectable } from '@nestjs/common';
+import { Card } from 'src/interfaces/card/card.interface';
 
 @Injectable()
-export class CardsService {}
+export class CardsService {
+  getCards(): Promise<Card[]> {
+    const cards = [
+      {
+        id: 1,
+        question: 'What is the language of the Web',
+        answer: 'JavaScript',
+      },
+      { id: 2, question: 'What is the coolest job', answer: 'Developper' },
+    ];
+    return new Promise((resolve) => {
+      resolve(cards);
+    });
+  }
+}
